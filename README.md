ee78579eb2c4c1f40f7f9b7903b1c9a91ac4c8ee
Paul Melnikow <email@paulmelnikow.com>
2025-05-07 13:42:20 -0400

    Initial version

---
+++ README.md
@@ -0,0 +1,15 @@
+# s3-sync-lambda
+
+![types](https://img.shields.io/badge/types-mypy-brightgreen?style=flat-square)
+
+Sync an S3 bucket to another S3 bucket in AWS Lambda.
+
+
+## Acknowledgements
+
+This Lambda function was initially developed by Jacob Beard.
+
+
+## License
+
+The project is licensed under the Apache License, Version 2.0.
---
+++ dev.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env -S poetry run python
+
+import os
+import click
+from executor import execute
+
+
+def python_source_files():
+    import glob
+
+    include_paths = (
+        glob.glob("*.py")
+        + glob.glob("s3_sync_lambda/*.py")
+        + glob.glob("s3_sync_lambda/**/*.py")
+    )
+    exclude_paths = []
+    return [x for x in include_paths if x not in exclude_paths]
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def install():
+    execute("poetry install --sync")
+
+
+@cli.command()
+def check_types():
+    execute("mypy", "--package", "missouri", "--show-error-codes")
+
+
+@cli.command()
+def lint():
+    execute("flake8", *python_source_files())
+
+
+@cli.command()
+def black():
+    execute("black", *python_source_files())
+
+
+@cli.command()
+def black_check():
+    execute("black", "--check", *python_source_files())
+
+
+@cli.command()
+def publish():
+    execute("rm -rf dist/")
+    execute("poetry build")
+    execute("twine upload dist/*")
+
+
+if __name__ == "__main__":
+    os.chdir(os.path.abspath(os.path.dirname(__file__)))
+    cli()
