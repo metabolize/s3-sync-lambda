c651bbd665da619242883f1da77ac9d41cc33112
Paul Melnikow <email@paulmelnikow.com>
2025-05-09 18:09:45 -0400

    More wip

--- s3_sync_lambda/testing/test_integration.py
+++ s3_sync_lambda/testing/test_integration.py
@@ -1,19 +1,45 @@
-import os
-import pytest
-from .testing_helpers import AWS_REGION
+from botocore.exceptions import ClientError
+from mypy_boto3_s3.client import S3Client
+from .testing_helpers import AWS_REGION, unique
+
+
+SOURCE_BUCKET = "metabolize-s3-sync-test-src"
+SOURCE_REGION = "us-east-1"
+TARGET_BUCKET = "metabolize-s3-sync-test-dst"
+TARGET_REGION = "us-west-1"
+
+
+def is_key_in_bucket(s3_client: S3Client, bucket: str, key: str) -> bool:
+    try:
+        s3_client.head_object(Bucket="your-bucket", Key="your-key")
+        return True
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "404":
+            return False
+        else:
+            raise
 
 
 def test_integration(deployed_function: str) -> None:
-    lambda_client = boto3.client("lambda", region_name=AWS_REGION)
+    import boto3
 
-    # Create a file on src
-    # Ensure it's not on dst
+    key = f"test-{unique()}.txt"
 
-    response = lambda_client.invoke(
+    s3_client = boto3.client("s3")
+
+    s3_client.bucket(SOURCE_BUCKET).put_object(
+        Key=key,
+        Body="This is a test file.",
+    )
+
+    # Confidence check.
+    assert not is_key_in_bucket(s3_client=s3_client, bucket=TARGET_BUCKET, key=key)
+
+    response = boto3.client("lambda", region_name=AWS_REGION).invoke(
         FunctionName=deployed_function,
         InvocationType="RequestResponse",
     )
     print("lambda response", response)
     print("payload", response["Payload"].read().decode("utf-8"))
 
-    # Assert that it's on dst
+    assert is_key_in_bucket(s3_client=s3_client, bucket=TARGET_BUCKET, key=key)
--- s3_sync_lambda/testing/testing_helpers.py
+++ s3_sync_lambda/testing/testing_helpers.py
@@ -8,7 +8,13 @@ MEMORY_SIZE = 3008
 AWS_REGION = "us-east-1"
 ROLE = os.environ["LAMBDA_ROLE"]
 TIMEOUT = 60
-S3_CODE_BUCKET = os.environ.get("UPLOAD_BUCKET", None)
+# S3_CODE_BUCKET = os.environ.get("UPLOAD_BUCKET", None)
+
+
+def unique() -> str:
+    import uuid
+
+    return uuid.uuid4().hex
 
 
 @pytest.fixture(scope="session")
@@ -16,19 +22,16 @@ def deployed_function(
     request: pytest.FixtureRequest, tmp_path_factory: pytest.TempPathFactory
 ) -> str:
     import os
-    import platform
-    import time
-    import uuid
     import boto3
     from werkit.aws_lambda.deploy import perform_create
 
     client = boto3.client("lambda")
-    function_name = f"{FUNCTION_PREFIX}_test_{uuid.uuid4().hex}"
+    function_name = f"{FUNCTION_PREFIX}_test_{unique()}"
 
-    def cleanup():
+    def cleanup() -> None:
         client.delete_function(FunctionName=function_name)
 
-    assert os.path.isfile(lambda_zip_path), f"Lambda zip not found: {LAMBDA_ZIP_PATH}"
+    assert os.path.isfile(LAMBDA_ZIP_PATH), f"Lambda zip not found: {LAMBDA_ZIP_PATH}"
 
     perform_create(
         aws_region=AWS_REGION,
@@ -37,8 +40,8 @@ def deployed_function(
         function_name=function_name,
         role=ROLE,
         timeout=TIMEOUT,
-        memory_size=WORKER_MEMORY_SIZE,
-        s3_code_bucket=S3_CODE_BUCKET,
+        memory_size=MEMORY_SIZE,
+        # s3_code_bucket=S3_CODE_BUCKET,
         verbose=True,
     )
 
